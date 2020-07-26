43fff8896b2637782231943ed96b525e9368b778
Diego Vicente <diego.vicente@ug.uchile.cl>
2020-07-26 18:50:44 -0400

    Eliminación de códigos no utilizados

--- js/index.js
+++ js/index.js
@@ -125,14 +125,14 @@ $(document).ready(function () {
             }
         } else if (month < 6 ) {
             value = $(this).val()
-            if (isBetween(value,25,40)) {
+            if (value<25) {
                 console.log("error")            
                 $("#fr_score").show()
                 $("#fr_score").text("No válido")
                 $(this).removeClass("is-valid").addClass("is-invalid")
                 ikctv.values.fr = null
                 ikctv.scores.fr = null
-            } else if (value==40) {
+            } else if (isBetween(value,25,40)) {
                 $("#fr_score").show()
                 $(this).removeClass("is-invalid").addClass("is-valid")
                 $("#fr_score").text("Puntaje: 0")
@@ -246,115 +246,6 @@ $(document).ready(function () {
     });
 
 
-    function sum(ft, e, wh) {
-        return ft + e + wh
-    };
-    function partial_total(p, s, a) {
-        return p * s * a
-    };
-    function validate(id, v, m) {
-        if (isBetween(v, 0, m)) {
-            $(id).removeClass("is-invalid").addClass("is-valid")
-            valid_form = true
-        } else if ($(id).val() == "") {
-            $(id).removeClass("is-invalid").removeClass("is-valid")
-            valid_form = false
-        } else {
-            $(id).removeClass("is-valid").addClass("is-invalid")
-            valid_form = false
-        }
-        return valid_form
-    };
-    function items(obj) {
-        var i, arr = [];
-        for (i in obj) {
-            arr.push(obj[i]);
-        }
-        return arr;
-    };
-    function calculate(part) {
-        p = ($("#" + part + "-percentage").text())
-        fissured_tongue = parseInt($("#" + part + "-fissured_tongue").val())
-        fissured_tongue_valid = validate("#" + part + "-fissured_tongue", fissured_tongue, 1)
-
-        erythema = parseInt($("#" + part + "-erythema").val())
-        erythema_valid = validate("#" + part + "-erythema", erythema, 4)
-
-        area_score = parseInt($("#" + part + "-area_score").val())
-        area_score_valid = validate("#" + part + "-area_score", area_score, 6)
-
-        white_halo = parseInt($("#" + part + "-white_halo").val())
-        white_halo_valid = validate("#" + part + "-white_halo", white_halo, 4)
-
-        if (fissured_tongue_valid & erythema_valid & white_halo_valid) {
-            part_sum = sum(
-                fissured_tongue,
-                erythema,
-                white_halo,
-            )
-            $("#" + part + "-sum").text(part_sum)
-        } else {
-            $("#" + part + "-sum").text("")
-        }
-
-        if (fissured_tongue_valid & erythema_valid & white_halo_valid & area_score_valid) {
-            part_partial_total = partial_total(
-                p,
-                part_sum,
-                area_score
-            )
-            $("#" + part + "-partial_total").text(Math.round(part_partial_total * 1000) / 1000)
-            total_values[part] = part_partial_total
-        } else {
-            $("#" + part + "-partial_total").text("")
-            total_values[part] = NaN
-        }
-    };
-    total_values = {
-        apex: NaN,
-        borders: NaN,
-        lingual_belly: NaN,
-        dorsum: NaN
-    };
-    $("input").change(function () {
-        calculate("apex")
-        calculate("borders")
-        calculate("lingual_belly")
-        calculate("dorsum")
-        total = items(total_values).reduce((a, b) => a + b, 0)
-        if (total) {
-            $("#result").show()
-            $("#total").text(Math.round(total * 1000) / 1000)
-            if (total >= 12) {
-                $("#score").text("Severe")
-            } else if (total >= 7) {
-                $("#score").text("Moderate")
-            } else {
-                $("#score").text("Mild")
-            }
-        } else {
-            $("#result").hide()
-        }
-    },
-    $('input[name=apa]').click(function(){
-        console.log('Hey you clicked this: ' + this.value);
-          
-        if(this.value == 'apa_0'){
-          rate_value = $('0').value;
-        } else if(this.value =='apa_1'){
-         rate_value = $('1').value;
-        } else if(this.value =='apa_2'){
-         rate_value = $('2').value;
-        }  
-      
-        $('#results').innerHTML = rate_value;
-    }),
-
-    $("input[type=file]").on("change", function () {
-        $("[for=file]").html(this.files[0].name);
-        $("#preview").attr("src", URL.createObjectURL(this.files[0]))
-    }))
-
     function calcular(a,b,c,d,e,f,g,h,i){
         s= a+b+c+d+e+f+g+h+i
         return s
@@ -383,7 +274,7 @@ $(document).ready(function () {
                 $("#result").show()
                 $("#result").css({'color':'red', 'font-weight': 'bold'});
                 $(this).removeClass("is-invalid").addClass("is-valid")
-                $("#result").text("con 3 puntos en RVA, 3 en UMA y/o FR>60 ciclos/minuto, en pacientes mayores de 6 meses y FR>70 ciclos/minuto en menores de 6 meses: sólo aspirar y examinar.")
+                $("#result").text("Con 3 puntos en RVA, 3 en UMA y/o FR>60 ciclos/minuto, en pacientes mayores de 6 meses y FR>70 ciclos/minuto en menores de 6 meses: sólo aspirar y examinar.")
             }                      
         } else if (ikctv.scores.fr != null && ikctv.scores.so!= null && ikctv.scores.apa!= null && ikctv.scores.brd!= null &&
             ikctv.scores.uma!= null && ikctv.scores.rva!= null && ikctv.scores.mectos!= null && ikctv.scores.pva!= null &&
@@ -408,7 +299,7 @@ $(document).ready(function () {
                 $("#result").show()
                 $("#result").css({'color':'red', 'font-weight': 'bold'});
                 $(this).removeClass("is-invalid").addClass("is-valid")
-                $("#result").text("con 3 puntos en RVA, 3 en UMA y/o FR>60 ciclos/minuto, en pacientes mayores de 6 meses y FR>70 ciclos/minuto en menores de 6 meses: sólo aspirar y examinar.")
+                $("#result").text("Con 3 puntos en RVA, 3 en UMA y/o FR>60 ciclos/minuto, en pacientes mayores de 6 meses y FR>70 ciclos/minuto en menores de 6 meses: sólo aspirar y examinar.")
             }        
         } else {
             $("#result").show()
@@ -418,35 +309,3 @@ $(document).ready(function () {
         }
     }
 
- //  function calcularscore(suma){
-        //debugger;
-    	//var suma = 0;
-		//for (let [key, value] of Object.entries(ikctv.scores)){
-			//console.log(key, value);
-		//	suma = suma + value;
-		//	console.log(suma);
-		//	$("#suma").text(suma)
-   //         if (isBetween(suma,0,9)){
-     //           $("#result").show()
-       //         $(this).removeClass("is-invalid").addClass("is-valid")
-         //       $("#result").css({'color':'green', 'font-weight': 'bold'});
-           //     $("#result").text("Examinar la necesidad de aplicar técnicas de kinesiterapia respiratoria.")
-                               
-          //  }else if (isBetween(suma,10,15)){
-            //    $("#result").show()
-              //  $(this).removeClass("is-invalid").addClass("is-valid")
-             //   $("#result").css({'color':'yellow', 'font-weight': 'bold'});
-               // $("#result").text("Realizar técnicas de kinesiterapia respiratoria y examinar la necesidad de aspiración de secreciones.")
- //           }else if (isBetween(suma,16,27)){
-   //             $("#result").show()
-     //           $("#result").css({'color':'red', 'font-weight': 'bold'});
-       //         $(this).removeClass("is-invalid").addClass("is-valid")
-         //       $("#result").text("con 3 puntos en RVA, 3 en UMA y/o FR>60 ciclos/minuto, en pacientes mayores de 6 meses y FR>70 ciclos/minuto en menores de 6 meses: sólo aspirar y examinar.")
-  //          }
-    //    }
-        
-   // }
-   //}
-  
-        
-  
